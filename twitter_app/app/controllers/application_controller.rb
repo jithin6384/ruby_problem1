e5bfb7a6d0b87f8efdc972d4cdc862fda1e41217
jithin <jeets2689@gmail.com>
2020-04-17 01:32:49 +0530

    twitter app

--- twitter_app/app/controllers/application_controller.rb
+++ twitter_app/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :signup_params, if: :devise_controller?
+
+  private
+   def signup_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+   end
 end
